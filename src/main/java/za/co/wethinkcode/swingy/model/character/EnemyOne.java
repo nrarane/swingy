595d3724edc0f7065adefe710085d0b68f3927e5
nrarane <nrarane@student.42.fr>
2018-08-21 17:07:06 +0200

    working on making sure all classes exist and in the correct packages

---
+++ src/main/java/za/co/wethinkcode/swingy/model/character/EnemyOne.java
@@ -0,0 +1,11 @@
+package za.co.wethinkcode.swingy.model.character;
+
+import javax.swing.text.Position;
+
+public class EnemyOne extends Enemy {
+
+    public EnemyOne(String name, int level, int experience, int attack, int hitPoints, int defence, Position position) {
+        super(name, level, experience, attack, hitPoints, defence, position);
+    }
+
+}
