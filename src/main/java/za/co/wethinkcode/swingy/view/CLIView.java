2a74c693945cf9d2430e8d914c8f694c25443cbb
nrarane <nrarane@student.42.fr>
2018-10-23 18:22:32 +0200

    almost done with cli

--- src/main/java/za/co/wethinkcode/swingy/view/CLIView.java
+++ src/main/java/za/co/wethinkcode/swingy/view/CLIView.java
@@ -49,7 +49,7 @@ public class CLIView {
             System.out.println("1. " + hero.name);
             System.out.println("2. " + hero2.name);
             System.out.println("3. " + hero3.name);
-            System.out.println("");
+            System.out.println();
             System.out.print("Your hero: ");
 
             Scanner characterInput = new Scanner(System.in);
@@ -58,74 +58,81 @@ public class CLIView {
             Hero[] heroes = {hero, hero2, hero3};
             Hero heroSelected = heroes[character - 1];
 
-            System.out.println("name: " + heroSelected.name + "\n" +
-                    "attack: " + heroSelected.attack + "\n" +
-                    "hp: " + heroSelected.hitPoints + "\n" +
-                    "xp: " + heroSelected.experience + "\n" +
-                    "defence: " + heroSelected.defence );
+            System.out.println( "name:    " + heroSelected.name + "\n" +
+                    "attack:  " + heroSelected.attack + "\n" +
+                    "hp:      " + heroSelected.hitPoints + "\n" +
+                    "xp:      " + heroSelected.experience + "\n" +
+                    "defence: " + heroSelected.defence + "\n" );
 
-            System.out.println("1. Continue\n2. Back\n3. Exit");
+            System.out.println("1. Continue\n2. Back\n3. Exit\n");
 
             Scanner choice = new Scanner(System.in);
+            System.out.print("Your option: ");
             int chose = choice.nextInt();
 
-            String[][] gridd = new String[5][5];
-
             System.out.println();
-            Point heroPos = new Point(5/2, 5/2);
 
-            movePlayer("", heroPos);
+            if (chose == 1) {
+                Point heroPos = new Point(5 / 2, 5 / 2);
+                for (int i = 0; i < 5; i++) {
+                    Point enemyPos = randXYPoint();
+                }
 
-            System.out.println();
-            while ((heroPos.x < 5 && heroPos.x > 0) || (heroPos.y > 0 && heroPos.y < 5)) {
+                movePlayer("", heroPos);
 
-//                int enemyNum = 0;
-//                while (enemyNum < 5) {
-//                    Point enemyPos = randXYPoint();
-//                    initEnemies(enemyPos);
-//                    enemyNum++;
-//                }
-//                if ((heroPos.x < 5 && heroPos.x > 0) || (heroPos.y > 0 && heroPos.y < 5)) {
-//
-//                }
+                System.out.println();
+                while ((heroPos.x > -1 && heroPos.x < 5) && (heroPos.y > -1 && heroPos.y < 5)) {
 
-                System.out.print("A: Left\nS: Down\nD: Right\nW: Up\n\n1: QuitSave\n2: Quit\n\nYour move: ");
+                    System.out.print("A: Left\nS: Down\nD: Right\nW: Up\n\n1: QuitSave\n2: Quit\n\nYour move: ");
 
-                Scanner moveHero = new Scanner(System.in);
-                String moveDirection = null;
-                int quitSave = 0;
+                    Scanner moveHero = new Scanner(System.in);
+                    String moveDirection = null;
+                    int quitSave = 0;
 
-                if (moveHero.hasNextBigInteger()) {
-                    quitSave = moveHero.nextInt();
+                    if (moveHero.hasNextBigInteger()) {
+                        quitSave = moveHero.nextInt();
 
-                    if (quitSave == 1) {
-                        System.out.println("you have gone to previous screen");
-                    } else if (quitSave == 2) {
-                        System.exit(0);
+                        if (quitSave == 1) {
+                            System.out.println("you have gone to previous screen");
+                        } else if (quitSave == 2) {
+                            System.exit(0);
+                        }
+                    } else if (moveHero.hasNext()) {
+                        moveDirection = moveHero.next();
+
+                        switch (moveDirection.toUpperCase()) {
+                            case "W":
+                                heroPos.x = heroPos.x - 1;
+                                break;
+                            case "A":
+                                heroPos.y = heroPos.y - 1;
+                                break;
+                            case "S":
+                                heroPos.x = heroPos.x + 1;
+                                break;
+                            case "D":
+                                heroPos.y = heroPos.y + 1;
+                                break;
+                            default:
+                                break;
+                        }
+
                     }
-                } else if (moveHero.hasNext()) {
-                    moveDirection = moveHero.next();
-
-                    switch (moveDirection.toUpperCase()) {
-                        case "W":
-                            heroPos.x = heroPos.x - 1;
-                            break;
-                        case "A":
-                            heroPos.y = heroPos.y - 1;
-                            break;
-                        case "S":
-                            heroPos.x = heroPos.x + 1;
-                            break;
-                        case "D":
-                            heroPos.y = heroPos.y + 1;
-                            break;
-                        default:
-                            break;
+                    if ((heroPos.x == -1 || heroPos.x == 5) || (heroPos.y == -1 || heroPos.y == 5)) {
+                        System.out.println("Change stage. \nIncrease level(dependent on the hp accumulated)\n");
+                        break;
                     }
+                    movePlayer(moveDirection, heroPos);
 
                 }
-                movePlayer(moveDirection, heroPos);
 
+            } else if (chose == 2) {
+                System.out.println("HERE WE SHOULD DISPLAY PREVIOUS OPTIONS");
+                System.exit(0);
+            } else if (chose == 3) {
+                System.exit(0);
+            } else {
+                System.exit(0);
             }
 
         } else if (option == 2) {
@@ -136,47 +143,52 @@ public class CLIView {
 
             System.exit(0);
 
+        } else {
+             System.out.println("You have to select 1 option");
         }
 
     }
 
-    public void drawGrid() {
-        String[][] board = new String[5][5];
-
-        for (int i = 0; i < 5/*mapSize*/; i++ ) {
-            for (int j = 0; j < 5/*mapSize*/; j++) {
-                board[i][j] = "*";
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+//    public void drawGrid() {
+//        String[][] board = new String[5][5];
+//
+//        for (int i = 0; i < 5/*mapSize*/; i++ ) {
+//            for (int j = 0; j < 5/*mapSize*/; j++) {
+//                board[i][j] = "*";
+//                System.out.print(board[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
 
     public void movePlayer(String direction, Point heroPos)  {
         String[][] board = new String[5][5];
+        int count = 0;
 
         for (int i = 0; i < 5/*mapSize*/; i++ ) {
             for (int j = 0; j < 5/*mapSize*/; j++) {
                 board[i][j] = "*";
                 board[heroPos.x][heroPos.y] = "H";
+//                initEnemies(board);
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+
     }
 
-    public void initEnemies(Point enemyPos) {
+    public void initEnemies(String[][] board) {
 
+//        int count = 0;
 //        enemyPos = randXYPoint();
-        String[][] board = new String[5][5];
 
         for (int i = 0; i < 5/*mapSize*/; i++ ) {
-            for (int j = 0; j < 5/*mapSize*/; j++) {
-                board[i][j] = "*";
-                board[enemyPos.x][enemyPos.y] = "E";
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+//            for (int j = 0; j < 5/*mapSize*/; j++) {
+            Point enemyPos = randXYPoint();
+//            System.out.println(enemyPos);
+            board[enemyPos.x][enemyPos.y] = "E";
+//            }
+
         }
 
     }
